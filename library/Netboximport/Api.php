7d5c554b7e18e226d98a517ade29f1c90569e573
Tim Forbes <tforbes@digitalocean.com>
2018-09-21 18:28:36 -0400

    Ran through beautifier, add pagination recursion, some cleanup.

--- library/Netboximport/Api.php
+++ library/Netboximport/Api.php
@@ -2,66 +2,108 @@
 
 namespace Icinga\Module\Netboximport;
 
-class Api {
-    function __construct($baseurl, $apitoken) {
+class Api
+{
+    public function __construct($baseurl, $apitoken)
+    {
         $this->baseurl = rtrim($baseurl, '/') . '/';
         $this->apitoken = $apitoken;
         $this->cache = [];
     }
 
-    private static function startsWith($haystack, $needle) {
-         return (substr($haystack, 0, strlen($needle)) === $needle);
-    }
+    // private static function startsWith($haystack, $needle) {
+    //      return (substr($haystack, 0, strlen($needle)) === $needle);
+    // }
 
-    private function apiRequest($method, $url, $get_params) {
-        if ($this->startsWith($url, $this->baseurl)) {
-            $url = substr($url, strlen($this->baseurl));
-        } else if ($this->startsWith(preg_replace("/^http:/i", "https:", $url), $this->baseurl)) {
-            $url = substr($url, strlen($this->baseurl)-1);
-        } else if ($this->startsWith(preg_replace("/^https:/i", "http:", $url), $this->baseurl)) {
-            $url = substr($url, strlen($this->baseurl)+1);
-        }
-
+    private function setupCurl()
+    {
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-
-        $get_params['limit'] = 1000000;
-
-        $query = http_build_query($get_params);
-        curl_setopt($ch, CURLOPT_URL, $this->baseurl . trim($url, '/') . '/?' . $query);
+        // Configure curl
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // curl_exec returns response as a string
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Follow redirect requests
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 5); // limit number of redirects to follow
 
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             'Authorization: Token ' . $this->apitoken,
         ));
 
-        if($method == 'POST') {
-            curl_setopt($ch, CURLOPT_POST, 1);
-        } elseif ($method == 'PUT') {
-            curl_setopt($ch, CURLOPT_PUT, 1);
-        } else {
-            // defaults to GET
+        return $ch;
+    }
+
+    private function apiRequest($method, $url, $get_params, $ch = null)
+    {
+        // if ($this->startsWith($url, $this->baseurl)) {
+        //     $url = substr($url, strlen($this->baseurl));
+        // } else if ($this->startsWith(preg_replace("/^http:/i", "https:", $url), $this->baseurl)) {
+        //     $url = substr($url, strlen($this->baseurl)-1);
+        // } else if ($this->startsWith(preg_replace("/^https:/i", "http:", $url), $this->baseurl)) {
+        //     $url = substr($url, strlen($this->baseurl)+1);
+        // }
+
+        //  This module should only ever pull information from netbox, right?
+        // if($method == 'POST') {
+        //     curl_setopt($ch, CURLOPT_POST, 1);
+        // } elseif ($method == 'PUT') {
+        //     curl_setopt($ch, CURLOPT_PUT, 1);
+        // } else {
+        //     // defaults to GET
+        // }
+
+        // Create curl object if necessary
+        if (!isset($ch)) {
+            $ch = $this->setupCurl();
         }
 
-        $response = curl_exec($ch);
+        $url_path = parse_url($url, PHP_URL_PATH);
+
+        // Strip '/api' since it's included in $this->baseurl
+        $url_path = preg_replace("|^/api/|", "/", $url_path);
+
+        // This is limited by MAX_PAGE_SIZE (https://netbox.readthedocs.io/en/stable/configuration/optional-settings/#max_page_size)
+        $get_params['limit'] = 1001;
+
+        // Convert parameters to URL-encoded query string
+        $query = http_build_query($get_params);
+
+        // Tie it all together
+        $uri = $this->baseurl . trim($url_path, '/') . '/?' . $query;
+
+        curl_setopt($ch, CURLOPT_URL, $uri);
+
+        $response = curl_exec($ch); // CURLOPT_RETURNTRANSFER makes this a string
         $curlerror = curl_error($ch);
-        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $status = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
 
         if ($curlerror === '' && $status === 200) {
             $response = json_decode($response);
 
-            if(isset($response->results)) {
-                return $response->results; // collection
-            } else {
-                return $response; // single
+            if (!isset($response->results)) { // single object
+                return $response;
+            } elseif (isset($response->next)) { // paginated results
+                // more results
+                // array_merge($response->results, apiRequest($method, $url, $get_params, $ch)); // recursion
+                $all_results = array_merge(
+                $response->results,
+                $this->apiRequest($method, $response->next, $get_params, $ch)
+              );
+                return $all_results;
+            } elseif (!isset($response->next)) { // end of pagination or single page
+                return $response->results;
             }
+
+            // if(isset($response->results)) {
+            //     return $response->results; // collection
+            // } else {
+            //     return $response; // single
+            // }
         } else {
-            throw new \Exception("Netbox API request failed: status=$status, error=$curlerror");
+            throw new \Exception("Netbox API request failed: uri=$uri; status=$status; error=$curlerror");
         }
     }
 
-    public function g($resource, $filter=array(), $cache=TRUE) {
+    public function g($resource, $filter=array(), $cache=true)
+    {
         $cache_key = sha1($resource . json_encode($filter));
 
         if (isset($this->cache[$cache_key])) {
--- library/Netboximport/ProvidedHook/Director/ImportSource.php
+++ library/Netboximport/ProvidedHook/Director/ImportSource.php
@@ -7,36 +7,42 @@ use Icinga\Module\Director\Web\Form\QuickForm;
 use Icinga\Module\Director\Hook\ImportSourceHook;
 use Icinga\Module\Netboximport\Api;
 
-class ImportSource extends ImportSourceHook {
+class ImportSource extends ImportSourceHook
+{
     private $api;
     private $resolve_properties = [
         "cluster",
     ];
 
-    private static function endsWith($haystack, $needle) {
+    private static function endsWith($haystack, $needle)
+    {
         $length = strlen($needle);
         return $length === 0 || (substr($haystack, -$length) === $needle);
     }
 
     // stolen from https://stackoverflow.com/a/9546235/2486196
     // adapted to also flatten nested stdClass objects
-    function flattenNestedArray($prefix, $array, $delimiter="__") {
+    public function flattenNestedArray($prefix, $array, $delimiter="__")
+    {
         $result = [];
 
         foreach ($array as $key => $value) {
-            if (is_object($value))
+            if (is_object($value)) {
                 $value = get_object_vars($value);
+            }
 
-            if (is_array($value))
+            if (is_array($value)) {
                 $result = array_merge($result, $this->flattenNestedArray($prefix . $key . $delimiter, $value, $delimiter));
-            else
+            } else {
                 $result[$prefix . $key] = $value;
+            }
         }
 
         return $result;
     }
 
-    private function fetchObjects($ressource, $activeOnly, $additionalKeysCallback = null) {
+    private function fetchObjects($ressource, $activeOnly, $additionalKeysCallback = null)
+    {
         $objs = $this->api->g($ressource);
         $objs = array_filter($objs, function ($o) use ($activeOnly) {
             return
@@ -54,7 +60,7 @@ class ImportSource extends ImportSourceHook {
 
             $o = $this->flattenNestedArray('', $o);
 
-            if(is_callable($additionalKeysCallback)) {
+            if (is_callable($additionalKeysCallback)) {
                 $keys = $additionalKeysCallback($o['id']);
                 $o = array_merge($o, $keys);
             }
@@ -72,7 +78,8 @@ class ImportSource extends ImportSourceHook {
         return $objs;
     }
 
-    private function fetchHosts($url, $type, $activeonly) {
+    private function fetchHosts($url, $type, $activeonly)
+    {
         $hosts = $this->fetchObjects($url, $activeonly, function ($id) use ($type) {
             return $this->flattenNestedArray('', [
                 'interfaces' => $this->interfaces[$type][$id] ?? []
@@ -81,7 +88,8 @@ class ImportSource extends ImportSourceHook {
         return $hosts;
     }
 
-    private function fetchInterfaces() {
+    private function fetchInterfaces()
+    {
         $ips = $this->api->g('ipam/ip-addresses');
 
         $owners = [
@@ -90,18 +98,18 @@ class ImportSource extends ImportSourceHook {
         ];
         $owner_types = array_keys($owners);
 
-        foreach($ips as $ip) {
-            if(!$ip->interface) {
+        foreach ($ips as $ip) {
+            if (!$ip->interface) {
                 continue;
             }
 
             $ifname = strtolower($ip->interface->name);
 
-            if($ifname === 'lo') {
+            if ($ifname === 'lo') {
                 continue;
             }
 
-            foreach($owner_types as $ot) {
+            foreach ($owner_types as $ot) {
                 if ($ip->interface->$ot) {
                     $owner_type = $ot;
                     $owner_id = $ip->interface->$ot->id;
@@ -125,7 +133,8 @@ class ImportSource extends ImportSourceHook {
         return $owners;
     }
 
-    public static function addSettingsFormFields(QuickForm $form) {
+    public static function addSettingsFormFields(QuickForm $form)
+    {
         $form->addElement('text', 'baseurl', array(
             'label'       => $form->translate('Base URL'),
             'required'    => true,
@@ -158,7 +167,8 @@ class ImportSource extends ImportSourceHook {
         ));
     }
 
-    public function fetchData() {
+    public function fetchData()
+    {
         $baseurl = $this->getSetting('baseurl');
         $apitoken = $this->getSetting('apitoken');
         $activeonly = $this->getSetting('activeonly') === 'y';
@@ -168,23 +178,25 @@ class ImportSource extends ImportSourceHook {
 
         $objects = [];
 
-        if($this->getSetting('importdevices') === 'y') {
+        if ($this->getSetting('importdevices') === 'y') {
             $objects[] = $this->fetchHosts('dcim/devices', 'device', $activeonly);
         }
 
-        if($this->getSetting('importvirtualmachines') === 'y') {
+        if ($this->getSetting('importvirtualmachines') === 'y') {
             $objects[] = $this->fetchHosts('virtualization/virtual-machines', 'virtual-machine', $activeonly);
         }
 
         return array_merge(...$objects);
     }
 
-    public function listColumns() {
+    public function listColumns()
+    {
         // return a list of all keys, which appeared in any of the objects
         return array_keys(array_merge(...array_map('get_object_vars', $this->fetchData())));
     }
 
-    public function getName() {
+    public function getName()
+    {
         return 'Netbox';
     }
 }
